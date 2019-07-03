328f2455f80565f6fe4a967a935725649e50884a
kwisedchart <krittanon.wisedchart@agoda.com>
2019-07-03 13:27:00 +0700

    add select input

--- simple-1.js
+++ simple-1.js
@@ -1,13 +1,24 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
 (async () => {
   // enable ui
-  const browser = await puppeteer.launch({headless: false})
+  const browser = await puppeteer.launch({
+    // executablePath: "./node_modules/puppeteer/.local-chromium/linux-656675/chrome-win/chrome.exe",
+    // executablePath: "./node_modules/puppeteer/.local-chromium/win64-656675/chrome-win/chrome.exe",
+    headless: false
+  })
   // disable ui
   // const browser = await puppeteer.launch();
   const page = await browser.newPage()
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  })
   await page.goto('https://google.com')
+  await page.waitFor('input[name=q]')
+  await page.$eval('input[name=q]', el => el.value = 'test test test')
   await page.screenshot({path: 'google.png'})
 
   await browser.close()
-})()
+})();
