893bc52570acf9b712d7f3c1f55229658b719fd7
kwisedchart <Krittanon.Wisedchart@agoda.com>
2019-07-03 14:24:49 +0700

    refact config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# puppeteer-self-training
+# puppeteer-poc
+
+## What's new
+https://www.youtube.com/watch?v=MbnATLCuKI4
+
+## required
+- linux, wsl
+  * apt-get install chromium-browser
--- simple-1.js
+++ simple-1.js
@@ -1,14 +1,27 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  // enable ui
-  const browser = await puppeteer.launch({
-    // executablePath: "./node_modules/puppeteer/.local-chromium/linux-656675/chrome-win/chrome.exe",
-    // executablePath: "./node_modules/puppeteer/.local-chromium/win64-656675/chrome-win/chrome.exe",
-    headless: false
-  })
-  // disable ui
-  // const browser = await puppeteer.launch();
+  const browserConfig = {
+    // executablePath: "./node_modules/puppeteer/.local-chromium/win64-656675/chrome-win/chrome.exe", //win64
+    headless: true,
+  }
+
+  const isWindowsPlatfrom = () => {
+    return process.platform.indexOf("win") >= 0
+  }
+
+  if (isWindowsPlatfrom() == false) {
+    // executablePath: "./node_modules/puppeteer/.local-chromium/linux-656675/chrome-linux/chrome",
+    // executablePath: "/usr/bin/chromium-browser", //wsl
+    browserConfig.args = ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+
+  console.log("browser.config", browserConfig)
+  const browser = await puppeteer.launch(browserConfig)
+
+  console.log("process.platform", process.platform)
+  console.log("browser.version", await browser.version())
+
   const page = await browser.newPage()
   await page.setViewport({
     width: 1920,
@@ -17,8 +30,9 @@ const puppeteer = require('puppeteer');
   })
   await page.goto('https://google.com')
   await page.waitFor('input[name=q]')
-  await page.$eval('input[name=q]', el => el.value = 'test test test')
+  await page.$eval('input[name=q]', el => el.value = 'running date: ' + new Date().toUTCString())
   await page.screenshot({path: 'google.png'})
 
+  // close
   await browser.close()
 })();
